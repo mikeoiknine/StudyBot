26b91b2ada37971419159d24af671798c45bedd8
mikeoiknine <mikeoiknine@gmail.com>
2021-03-20 16:29:23 -0400

    Add CSV Course Parser
    Parse the course information from the CSV files found in the data/CSV/ directory.

---
+++ constants.py
@@ -0,0 +1,9 @@
+# Paths
+COURSE_DATA_CSV = "./data/CSV/course_catalog.csv"
+COURSE_DESCRIPTIONS_CSV = "./data/CSV/course_descriptions.csv"
+
+# Constants
+CSV_KEYS = ["Course ID", "Subject", "Catalog", "Long Title"]
+DESCRIPTION_PLACEHOLDERS = [
+    "Please see Graduate Calendar", "Please see GRAD Calendar", "Please see UGRD Calendar", "Please see Undergraduate Calendar"
+]
