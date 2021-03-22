6391056a07c6a340d990d831a630060d351e9025
mikeoiknine <mikeoiknine@gmail.com>
2021-03-21 20:38:25 -0400

    Add Parser for local courses
    - Added course data in the data/courses dir for the Comp 474 material.
    - Added a parser for the courses in the data/courses dir

--- constants.py
+++ constants.py
@@ -1,9 +1,12 @@
 # Paths
 COURSE_DATA_CSV = "./data/CSV/course_catalog.csv"
 COURSE_DESCRIPTIONS_CSV = "./data/CSV/course_descriptions.csv"
+COURSE_DATA = "./data/Courses"
 
 # Constants
 CSV_KEYS = ["Course ID", "Subject", "Catalog", "Long Title"]
 DESCRIPTION_PLACEHOLDERS = [
     "Please see Graduate Calendar", "Please see GRAD Calendar", "Please see UGRD Calendar", "Please see Undergraduate Calendar"
 ]
+LECTURE_KEYS = ["Slides", "Worksheets",
+                "Readings", "Labs", "Tutorials"]
--- course_parser.py
+++ course_parser.py
@@ -1,6 +1,7 @@
 import pandas as pd
+import os
 
-from constants import COURSE_DATA_CSV, COURSE_DESCRIPTIONS_CSV, CSV_KEYS, DESCRIPTION_PLACEHOLDERS
+from constants import COURSE_DATA_CSV, COURSE_DESCRIPTIONS_CSV, CSV_KEYS, DESCRIPTION_PLACEHOLDERS, COURSE_DATA, LECTURE_KEYS
 
 
 def parse_courses():
@@ -38,3 +39,67 @@ def parse_courses():
         course.pop("Course ID", None)
 
     return courses
+
+
+def get_paths(base_path, key):
+    """
+    Explores the path formed by appending `key` to `base_path` and returning a list of 
+    all the files found.
+
+    If no such path exists, None is returned.
+    """
+    path = os.path.join(base_path, key)
+    if not os.path.exists(path):
+        return None
+    return [os.path.join(path, f) for f in os.listdir(path)]
+
+
+def parse_local_courses():
+    """
+    Parse the course data in the structured directories in the COURSE_DATA path.
+
+    Returns a list of dictionaries containing the courses found in the path.
+    """
+    courses = []
+    abs_course_data_path = os.path.abspath(COURSE_DATA)
+    for course_path in os.listdir(abs_course_data_path):
+        course = {}
+
+        name, number = course_path.split()[0],  course_path.split()[1]
+        course["Subject"], course["Number"] = name, number
+
+        # Use absolute paths for individual resources
+        course_path = os.path.join(abs_course_data_path, course_path)
+
+        # Check for course outline
+        course["Outlines"] = get_paths(course_path, "Outlines")
+
+        # Explore lectures
+        lectures = parse_course_lectures(course_path)
+        course["Lectures"] = lectures
+        courses.append(course)
+
+    return courses
+
+
+def parse_course_lectures(course_path):
+    """
+    Parse the lecture data of a given course
+    """
+    lectures = []
+
+    base_lecture_path = os.path.join(course_path, "Lectures")
+    for lecture_dir in os.listdir(base_lecture_path):
+        lecture = {}
+
+        # Extract Lecture metadata
+        s = lecture_dir.split()
+        name, number = '_'.join(s[3:]), s[1]
+        lecture["Name"], lecture["Number"] = name, number
+
+        # Add all of the file paths found in the lecture_path subdirectory
+        lecture_path = os.path.join(base_lecture_path, lecture_dir)
+        for key in LECTURE_KEYS:
+            lecture[key] = get_paths(lecture_path, key)
+        lectures.append(lecture)
+    return lectures
