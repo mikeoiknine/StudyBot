ccb66c1309336de0f93916c157922fb2d5f9e2c1
mikeoiknine <mikeoiknine@gmail.com>
2021-03-23 17:57:36 -0400

    Build Graph from all available data

--- constants.py
+++ constants.py
@@ -10,3 +10,12 @@ DESCRIPTION_PLACEHOLDERS = [
 ]
 LECTURE_KEYS = ["Slides", "Worksheets",
                 "Readings", "Labs", "Tutorials"]
+
+# Hardcoded University dbpedia reference
+CONCORDIA_UNIVERSITY_DBPEDIA_URL = "https://dbpedia.org/resource/Concordia_University"
+
+# Hardcoded course urls
+COURSE_URLS = {
+    "COMP_474": "http://concordia.catalog.acalog.com/preview_course_nopop.php?catoid=1&coid=2718",
+    "COMP_445": "http://concordia.catalog.acalog.com/preview_course_nopop.php?catoid=1&coid=2713"
+}
