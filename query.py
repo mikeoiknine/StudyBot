bbca942b33a8c7120d68a785626ef49e188d2ad9
Thomas Gauvin <35609369+thomasgauvin@users.noreply.github.com>
2021-04-20 11:12:48 -0400

    Added competency questions 7,9,10  to rasa chatbot

--- query.py
+++ query.py
@@ -8,6 +8,7 @@ DBP = Namespace("http://dbpedia.org/property/")
 DCE = Namespace("http://purl.org/dc/elements/1.1/")
 VIVO = Namespace("http://vivoweb.org/ontology/core#")
 RDFS = Namespace("http://www.w3.org/2000/01/rdf-schema#")
+RDF = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
 
 def get_course_description(graph, course):
     """
@@ -167,4 +168,126 @@ def get_courses_covering_topic(graph, topicname):
 
         if course is not None:
             courses.append((str(course), coursesubject, coursenumber, coursename, doccount))
+    return courses
+
+def slides_for_lectures_covering_topic(graph, topicname):
+    """
+    Query the graph for slides of lectures covering topic X
+    """
+
+    print(topicname)
+
+    q = prepareQuery(
+        """SELECT ?slides
+            WHERE {
+                ?lec rdf:type focu:Lecture.
+                ?lec focu:slides ?slides .
+                ?slides focu:topics ?topic.
+                ?topic dbp:name ?topicname .
+            }""",
+        initNs = {
+            "focu": FOCU,
+            "rdf": RDF,
+            "dbp": DBP
+        }
+    )
+    
+    rows = list(graph.query(q, initBindings={"topicname": Literal(topicname.lower())}))
+    if rows is None or not rows:
+        return None 
+
+    slidesets = []
+    for row in rows:
+        slides = row.get("slides", None)
+        if slides is not None:
+            slidesets.append((str(slides)))
+    return slidesets
+
+def get_courses_no_lecture_slides(graph):
+    """
+    Query the graph for lectures without lecture slides
+    """
+ 
+    q = prepareQuery(
+        """SELECT DISTINCT ?course
+            WHERE {
+                ?course a vivo:Course.
+                OPTIONAL {
+                ?course focu:lectures ?lectures .
+                ?lectures focu:slides ?slides .
+            }
+            FILTER (!BOUND(?slides))
+            }
+            """,
+        initNs = {
+            "vivo": VIVO,
+            "focu": FOCU,
+            "rdf": RDF,
+            "dbp": DBP
+        }
+    )
+    
+    rows = list(graph.query(q))
+    if rows is None or not rows:
+        return None 
+
+    courses = []
+    for row in rows:
+        course = row.get("course", None)
+        if course is not None:
+            courses.append((str(course)))
+    return courses
+
+def courses_from_uni_covering_topic(graph, topicname, university):
+    """
+    Query the graph for slides of lectures covering topic X
+    """
+
+    print(topicname)
+    print(university)
+
+    q = prepareQuery(
+        """SELECT ?course ?coursesubject ?coursenumber ?coursename 
+        WHERE {{ 
+                ?course a vivo:Course; 
+                        dce:subject ?coursesubject;
+                        dbp:number ?coursenumber;
+                        dbp:name ?coursename;
+                        focu:topics ?topic.
+                ?topic a focu:Topic;
+                        dbp:name "{topic_name}".
+                ?uni a dbo:University;
+                    dbp:name "{uniname}";
+                    focu:courses ?course.
+             }}
+            GROUP BY ?course ?coursesubject ?coursenumber ?coursename
+        """.format(uniname= university, topic_name= topicname),
+        initNs = {
+            "focu": FOCU,
+            "rdf": RDF,
+            "dbp": DBP,
+            "dbo": DBO,
+            "vivo": VIVO,
+            "dce": DCE
+        }
+    )
+    
+    rows = list(graph.query(q, initBindings={
+        "topicname": Literal(topicname.lower()), 
+        "uniname": Literal(university.lower())
+        }))
+    print(list(rows))
+    if rows is None or not rows:
+        return None 
+
+
+    courses = []
+    for row in rows:
+        course = row.get("course", None)
+        coursesubject = row.get("coursesubject", None)
+        coursenumber = row.get("coursenumber", None)
+        coursename = row.get("coursename", None)
+
+        if course is not None:
+            courses.append((str(course), coursesubject, coursenumber, coursename))
     return courses
